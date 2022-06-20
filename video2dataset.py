1b620befa706387fddf9e2f8270f8e0670739568
faheem <faheem@dailyprophet.ai>
2022-06-20 09:43:26 +0530

    path correction

--- video2dataset.py
+++ video2dataset.py
@@ -4,8 +4,8 @@ success,image = vidcap.read()
 count = 0
 while success:
     try:
-        cv2.imwrite("cctv_small/frame%d.jpg" % count, image)
-        cv2.imwrite("lpd_output_small/frame%d.jpg" % count, image)
+        cv2.imwrite("cctv/frame%d.jpg" % count, image)
+        cv2.imwrite("lpd_output/frame%d.jpg" % count, image)
         success,image = vidcap.read()
         count += 1
     except:
